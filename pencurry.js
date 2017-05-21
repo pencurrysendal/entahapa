799d9a47287ad68a57e11b2760972a6a7c3b0bae
pencurrysendal <pencurrysendal@gmail.com>
2017-05-21 22:01:58 +0700

    Create pencurry.js

---
+++ pencurry.js
@@ -0,0 +1,8 @@
+function pencurry() {
+var sendal = prompt("Hello World!\n What is your name?","");
+if(sendal != null) {
+pencurrysendal = document.createElement("span");
+pencurrysendal.style.color = "#f00";
+pencurrysendal.innerHTML = "Hello" + sendal + ", welcome";
+}
+}
